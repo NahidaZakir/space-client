9a9ef6bfa8b39c670d398e240b4cfbf6a73d29e9
Nahida Zakir <nahidazakir15@gmail.com>
2022-11-09 16:56:48 +0600

    banner plus services shown

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import { FaMobileAlt } from 'react-icons/fa';
+import serviceBg from '../../Assets/service-bg.jpg'
 const Home = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div >
 
@@ -9,6 +16,24 @@ const Home = () => {
                 <h3 className='text-4xl md:text-9xl mt-10 mb-5 font-bold'>Cleaning your <br></br>worries away</h3>
                 <div className='flex justify-center'>
                     <button className='btn  w-2/5 text-xl mt-8'><FaMobileAlt className='text-xl'></FaMobileAlt>  Call Us: 01861260597</button>
+                </div>
+            </div>
+
+            <div className='w-full bg-violet-400'>
+                <h1 className='text-5xl text-center text-base-100 pt-10 pb-10 font-bold'>Our Services</h1>
+                <div className='grid grid-rows-1 grid-col-1 md:grid-col-2'>
+                    <div >
+                        <ul className='text-center font-semibold'>
+                            {
+                                services.map(service => <li className='text-3xl'>{service.name}</li>)
+                            }
+                        </ul>
+                    </div>
+                    <div>
+                        <img src={serviceBg} alt="" />
+                    </div>
+
+
                 </div>
 
             </div>
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setServices(data);
+            })
+
+    }, [])
     return (
         <div>
+            <h1>this is service</h1>
+            {
+                services.map(service => <p>{service.name}</p>)
+            }
 
         </div>
     );
